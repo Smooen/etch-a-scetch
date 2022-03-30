f82f02c9657abc5173c135a3c3c45e139062d76e
Smooen <smooenus@gmail.com>
2022-03-30 12:40:15 +0200

    Fixed gridlayout

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const divContainer = document.getElementById("container");
 
 
-for(let i = 0; i <= 16; i++){
-    for(let j = 0; j <= 16; j++){
+for(let i = 0; i < 16; i++){
+    for(let j = 0; j < 16; j++){
         let grid = document.createElement('div');
         grid.className = "grid";
         divContainer.appendChild(grid);
