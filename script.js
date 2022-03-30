eba4ae842a82eecdae1e738124f058e2f634d057
Smooen <smooenus@gmail.com>
2022-03-30 17:50:42 +0200

    Add resize option

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const divContainer = document.getElementById("container");
 
 const clearButton = document.getElementById("clearButton");
 
+const resizeButton = document.getElementById("resizeButton");
+
 // let size = prompt("Enter amount of Squares per side");
-let size = 20;
+let initSize = 20;
 
 function calcSize(size) {
     let sqaureSize = 600 / size;
@@ -11,29 +13,42 @@ function calcSize(size) {
 }
 
 
-
-function createCanvas() {
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            let grid = document.createElement('div');
-            grid.className = "grid";
-            grid.style.height = calcSize(size);
-            grid.style.width = calcSize(size);
-            grid.addEventListener('mouseover', () => {
-                grid.style.backgroundColor = "white";
-            });
-            divContainer.appendChild(grid);
-        }
+function createCanvas(size) {
+    for (let i = 0; i < size * size; i++) {
+        let grid = document.createElement('div');
+        grid.className = "grid";
+        grid.style.height = calcSize(size);
+        grid.style.width = calcSize(size);
+        grid.addEventListener('mouseover', () => {
+            grid.style.backgroundColor = "white";
+        });
+        divContainer.appendChild(grid);
     }
 }
 
+createCanvas(initSize);
+
 clearButton.addEventListener('click', () => {
     let dirtyGrid = document.querySelectorAll('.grid');
-    for(let k = 0; k < dirtyGrid.length; k++){
+    for (let k = 0; k < dirtyGrid.length; k++) {
         dirtyGrid[k].style.backgroundColor = "black";
     }
 });
 
+resizeButton.addEventListener('click', function () {
+    resizeCanvas(50);
+});
 
-createCanvas();
-//clear-grid button
+function deleteCanvas(){
+    let canvas = document.querySelectorAll('.grid');
+    for(let i = 0; i < canvas.length; i++){
+        canvas[i].remove();
+    }
+}
+
+function resizeCanvas(newSize) {
+    console.log("??")
+    deleteCanvas();
+    createCanvas(newSize);
+}
+
