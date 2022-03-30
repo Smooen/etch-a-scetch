defe3896076d7312c2184ba6b8b962f0c072682d
Smooen <smooenus@gmail.com>
2022-03-30 13:45:42 +0200

    Add clearButton

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 const divContainer = document.getElementById("container");
 
+const clearButton = document.getElementById("clearButton");
 
-for(let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){
-        let grid = document.createElement('div');
-        grid.className = "grid";
-        grid.addEventListener('mouseover', () => {
-            grid.style.backgroundColor = "white";
-        });
-        divContainer.appendChild(grid);
+// let size = prompt("Enter amount of Squares per side");
+let size = 20;
+
+function calcSize(size) {
+    let sqaureSize = 600 / size;
+    return sqaureSize + 'px'
+}
+
+
+
+function createCanvas() {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let grid = document.createElement('div');
+            grid.className = "grid";
+            grid.style.height = calcSize(size);
+            grid.style.width = calcSize(size);
+            grid.addEventListener('mouseover', () => {
+                grid.style.backgroundColor = "white";
+            });
+            divContainer.appendChild(grid);
+        }
     }
 }
+
+clearButton.addEventListener('click', () => {
+    let dirtyGrid = document.querySelectorAll('.grid');
+    for(let k = 0; k < dirtyGrid.length; k++){
+        dirtyGrid[k].style.backgroundColor = "black";
+    }
+});
+
+
+createCanvas();
+//clear-grid button
